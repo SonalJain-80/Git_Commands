b7ce881ecd759c7de7ddff0ab77fc20fa9301229
I527373 <2022sp93080@wilp.bits-pilani.ac.in>
2023-03-04 20:58:09 +0530

    String was changed

--- StringDemo.java
+++ StringDemo.java
@@ -2,13 +2,13 @@ package javatraining;
 
 public class StringDemo {
     public static void main(String[] args) {
-        String str="Hello";
-        String st1="Hello";
+        String str="Venessa";
+        String st1="Venessa";
         st1.concat(" ols"); // but  st1=st1.concat(" ols"); will work
         System.out.println(str.hashCode());
         System.out.println(st1.hashCode());
         System.out.println(str==st1);
-        String st2=new String("Hello");  //it does not check if "Hello" is present in string pool or not, it creates a new string "Hello"
+        String st2=new String("Venessa");  //it does not check if "Hello" is present in string pool or not, it creates a new string "Hello"
         System.out.println(st1.hashCode());
         System.out.println(str==st2);
     }
